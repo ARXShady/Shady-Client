afe08599462e93cdcbfe9df1fc392d17ba67ab75
ARXShady <arxshady@gmail.com>
2015-05-06 18:10:38 -0700

    Create Wrapper.java

---
+++ me/arxshady/shady/utils/Wrapper.java
@@ -0,0 +1,10 @@
+package me.arxshady.shady.utils;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.FontRenderer;
+
+public class Wrapper {
+	
+	public static Minecraft mc = Minecraft.getMinecraft();
+	public static FontRenderer fr = Minecraft.getMinecraft().fontRendererObj;
+}
